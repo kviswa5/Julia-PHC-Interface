4ff6b6387dfe78e6f8a5db81876fa18ee5e39d38
Jan Verschelde <janv@uic.edu>
2021-02-04 12:18:51 -0600

    added types to the input arguments

--- write_system.jl
+++ write_system.jl
@@ -1,25 +1,25 @@
 """
-    write_system(filename,system)
+    write_system(filename::String, polynomials::Array{String,1})
 
-Input: filename - name of file to write the system
-       system - A polynomial system (array of strings of polynomials),
+Writes a polynomial system to file.
 
-Output: The polynomial system written to a file.
+Input: filename is the name of the file to write the polynomials to.
+       The array polynomials is the string representation of a system.
 
 Example:
 
      pols = [\"x^3 + 2*x*y - 1;\", \"x + y - 1;\"];
      write_system(\"example.txt\", pols)
 """
-function write_system(filename,system)
-    sys_nvar = size(system)[1]
+function write_system(filename::String, polynomials::Array{String,1})
+    sys_nvar = size(polynomials)[1]
     sys_npoly = sys_nvar
     f = open(filename, "w")
     s = "$sys_npoly"
     write(f,s)
     write(f,"\n")
     for i in 1:sys_nvar
-        str = system[i]
+        str = polynomials[i]
         write(f,str)
         write(f,"\n")
     end
