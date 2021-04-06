f1f456049daf3aa53a77fe31ca7a174c9132079b
kviswa5 <67428077+kviswa5@users.noreply.github.com>
2021-04-06 12:00:47 -0700

    Add files via upload

---
+++ JuliaPHC.jl
@@ -0,0 +1,15 @@
+module juliaPHC
+
+# export, using, import statements are usually here; we discuss these below
+
+include("check_semicolon.jl")
+include("extract_sols.jl")
+include("read_system.jl")
+include("runphc.jl")
+include("solve_system.jl")
+include("str2cmplx.jl")
+include("write_system.jl")
+
+end
+
+
