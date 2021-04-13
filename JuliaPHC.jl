948c310389605ff9eb3d739c96f5fe10c7b92490
Jan Verschelde <janv@uic.edu>
2021-04-13 12:34:28 -0500

    added documentation string, export statement, and test function to the JuliaPHC module

--- JuliaPHC.jl
+++ JuliaPHC.jl
@@ -1,15 +1,22 @@
+"""
+The module juliaPHC is a collection of functions to prepare the input
+and process the output of the executable phc of the software PHCpack.
+
+Exports `read_system`, `write_system`, `solve_system`, `extract_sols`,
+and `test`.
+
+To run the test in a Julia sesion, type
+
+    juliaPHC.test()
+"""
 module juliaPHC
 
-# export, using, import statements are usually here; we discuss these below
+export read_system, write_system, solve_system, extract_sols, test
 
-include("check_semicolon.jl")
-include("extract_sols.jl")
 include("read_system.jl")
-include("runphc.jl")
-include("solve_system.jl")
-include("str2cmplx.jl")
 include("write_system.jl")
+include("solve_system.jl")
+include("extract_sols.jl")
+include("test.jl")
 
 end
-
-
