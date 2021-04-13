786c826df685a59b03a23be9a0359e033ec1fe20
Jan Verschelde <janv@uic.edu>
2021-04-13 12:35:29 -0500

    removed the module name when calling the exported functions in test

--- test.jl
+++ test.jl
@@ -6,17 +6,17 @@ Runs a simple test on the solve_system ...
 function test()
     p = ["x^3 + 2*x*y - 1;", "x + y - 1;"];
     name = "example.txt"
-    runphc.write_system(name, p)
+    write_system(name, p)
     println("A test polynomial system :")
     for i=1:size(p, 1)
         println(p[i])
     end
-    n, q = runphc.read_system("example.txt")
+    n, q = read_system("example.txt")
     println(n, " polynomials read from file $name :")
     for i=1:size(q, 1)
         println(q[i])
     end
-    sols, startsys, startsols = runphc.solve_system(p)
+    sols, startsys, startsols = solve_system(p)
     println("The start system :")
     for i=1:size(startsys, 1)
         println(startsys[i])
