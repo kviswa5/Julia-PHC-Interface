6ac84f300bb67cd51a81720834cc20f7adecbd7e
Jan Verschelde <janv@uic.edu>
2021-06-22 13:53:42 -0500

    added debug flag to remove the temporary file

--- version_string.jl
+++ version_string.jl
@@ -2,8 +2,9 @@ using Dates
 using Random
 
 """
-    version_string(phcbin::String=\"phc\";
-                   tmpdir::String=\"/tmp\",
+    version_string(phcbin::String="phc";
+                   tmpdir::String="tmp",
+                   debug::Bool=false,
                    verbose::Bool=true)::String
 
 Returns the version string of executable phc.
@@ -13,6 +14,8 @@ The input arguments are
     phcbin is the location of the binary phc
 
     tmpdir is the folder for temporary files
+
+    debug is the flag to clean temporary files
 
     verbose is the verbose flag
 
@@ -24,10 +27,14 @@ The default location of the output file is /tmp.
 Run version_string(tmpdir=".") to change the output
 folder to the current folder.
 
+By default, the output file of phc --version is removed.
+If debug is true, then this output file is not removed.
+
 Run version_string(verbose=false) to turn off the verbose mode.
 """
 function version_string(phcbin::String="phc";
                         tmpdir::String="/tmp",
+                        debug::Bool=false,
                         verbose::Bool=true)::String
     moment = Dates.now()
     seed = Dates.value(moment)
@@ -53,6 +60,13 @@ function version_string(phcbin::String="phc";
 
     if verbose
         println("The version string : $result")
+    end
+
+    if !debug
+        if verbose
+            println("Removing $outfile ...")
+        end
+        rm(outfile)
     end
 
     return result
