807325a9ba311f5536c2e73368f88ebe4a8486a8
kviswa5 <67428077+kviswa5@users.noreply.github.com>
2021-07-22 14:47:40 -0700

    Add files via upload
    Updated write_solution with argument typing and documentation

--- write_solution.jl
+++ write_solution.jl
@@ -1,8 +1,14 @@
 using Printf
 """
 Writes a solution in character format to a phc Pack format, with an associated filename.
+Preconditions - 
+                
+Arguments - (1) filename is a file the writes to the current directory
+(2) solution is a string of solutions in phc format
+Outputs a solution file with the name in the current directory.
+Example:
 """
-function write_solution(filename,solution)
+function write_solution(filename::String,solution::String)
     f = open(filename, "w")
     temp_sol = extract_sols(solution)
     solution = deepcopy(temp_sol)
@@ -23,21 +29,22 @@ function write_solution(filename,solution)
     n_var = length(var_names)
     sort!(var_names)
     
-    temp_sol_str = "=========================\n"
+    temp_sol_str = "==========================================================================\n"
     varstr = ""
     for k = 1:length(solution)
-        str_1 = "solution "* string(k) * ": \n"
-        str_2 = "t : "* @sprintf("%17.15e",real(solution[k]["time"]))* @sprintf("%17.15e",imag(solution[k]["time"]))*"\n"
+        str_1 = "solution "* string(k) * " : \n"
+        str_2 = "t : "* @sprintf("%17.15e",real(solution[k]["time"])) * " " * @sprintf("%17.15e",imag(solution[k]["time"]))*"\n"
         str_3 = "m : "* @sprintf("%17.15e",solution[k]["multiplicity"]) * "\n"
         varstr = "the solution for t: \n"
         for i in 1:n_var
             temp_2 = " " * var_names[i] * " : " * @sprintf("%17.15e",real(temp_sol[k][var_names[i]])) * " " * @sprintf("%17.15e",imag(temp_sol[k][var_names[i]])) * "\n"
             varstr = varstr * temp_2
         end
-        str_4 = "==err : " * @sprintf("%17.15e",solution[k]["err"]) * " rco : " * @sprintf("%17.15e",solution[k]["rco"]) *" res : " * @sprintf("%17.15e",solution[k]["res"]) *  " == \n"
+         str_4 = "== err : " * @sprintf("%17.15e",solution[k]["err"]) * " = rco : " * @sprintf("%17.15e",solution[k]["rco"]) *" = res : " * @sprintf("%17.15e",solution[k]["res"]) *  " == \n"
         temp_sol_str  = temp_sol_str *str_1*str_2*str_3*varstr*str_4
     end
     sol_string =  temp_sol_str
     write(f,sol_string)
     close(f)
 end
+
