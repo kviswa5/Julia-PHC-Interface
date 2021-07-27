673b9a73aa070ebcd9c59f0728dfc6f6ad9f4dbe
Jan Verschelde <janv@uic.edu>
2021-07-27 12:00:19 -0500

    added solve.jl and testcyclic8.jl as the exported Julia programs

--- juliaPHC.jl
+++ juliaPHC.jl
@@ -11,17 +11,19 @@ See the documentation of version_string for optional arguments.
 module juliaPHC
 
 export version_string, read_system, write_system, solve_system
-export extract_sols, extract_solution, mixed_volume
+export extract_sols, extract_solution, mixed_volume, solve
 export test, testcyclic7, testkatsura6
 
 include("version_string.jl")
 include("read_system.jl")
 include("write_system.jl")
 include("solve_system.jl")
+include("solve.jl")
 include("extract_sols.jl")
 include("extract_solution.jl")
 include("test.jl")
 include("testcyclic7.jl")
+include("testcyclic8.jl")
 include("mixed_volume.jl")
 include("testkatsura6.jl")
 
